10310545edb2312ebfd976a89d08c024a37afbbd
zefrenchwan <cenalon@yahoo.fr>
2024-07-03 19:20:25 +0200

    second improved version of walkthrough

--- storage/sql/002_susers/007_walkthroughs.sql
+++ storage/sql/002_susers/007_walkthroughs.sql
@@ -54,7 +54,12 @@ create or replace procedure susers.init_walkthrough_structures()
 language plpgsql as $$
 declare 
 begin 
-    create temporaty table if not exists temp_walkthroughs (
+	create temporary table if not exists temp_authorized_graphs (
+        walkthrough_id text, 
+		graph_id text
+    );
+
+    create temporary table if not exists temp_walkthroughs (
         walkthrough_id text, 
         element_id text,
         element_operand text,
@@ -69,8 +74,8 @@ alter procedure susers.init_walkthrough_structures owner to upa;
 -- It assumes table temp_walkthroughs exists
 create or replace procedure susers.delete_values_for_walkthrough(p_walkthrough_id text) 
 language plpgsql as $$
-declare 
-begin 
+begin
+	delete from  temp_authorized_graphs where walkthrough_id = p_walkthrough_id;  
 	delete from  temp_walkthroughs where walkthrough_id = p_walkthrough_id; 
 end; $$;
 
@@ -79,7 +84,7 @@ alter procedure susers.delete_values_for_walkthrough owner to upa;
 
 -- susers.find_neighbors_for_walkthrough fills walkthrough table to find elements around given values
 -- for that walkthrough (provided with id).   
-create or replace susers.find_neighbors_for_walkthrough(p_user_login, p_walkthrough_id text, p_period text) 
+create or replace procedure susers.find_neighbors_for_walkthrough(p_user_login text, p_walkthrough_id text, p_period text) 
 language plpgsql as $$
 declare
 	l_current_height int;
@@ -87,71 +92,92 @@ declare
 begin 
 	-- first hight is 0
 	l_current_height = 0;
-	-- include parent relation for elements ONLY ONCE. 
-	-- Algorithm is:
-	-- find authorized graphs
-	-- find visible elements 
-	-- find parent relations of visible elements
-	-- exclude relation values that are not visible (either relation not visible or operand not visible)
-	-- insert remaining relation parents
-    with all_authorized_graphs as (
-        select AAG.graph_id
-        from susers.authorized_graphs AAG 
-        join susers.users USR on USR.user_id = AAG.auth_user_id 
-        where user_login = p_user_login
-    ) all_valid_elements as (
-		-- element is valid if its graph is visible
-		-- AND this element is active during period parameter 
-        select ELT.graph_id, ELT.element_id, 
-		case ELT.element_type when 1 then true when 2 then false when 10 then true else false end as is_entity 
-        from sgraphs.elements ELT
-        join temp_walkthroughs TWA on TWA.element_id = ELT.element_id
+
+	insert into temp_authorized_graphs(walkthrough_id, graph_id)
+	select p_walkthrough_id, AAG.graph_id
+	from susers.authorized_graphs AAG 
+	join susers.users USR on USR.user_id = AAG.auth_user_id 
+	where user_login = p_user_login;
+
+	-- delete elements that are NOT visible from said user. 
+	-- We keep inactive elements, for user to deal with it. 
+	with all_authorized_graphs as (
+        select TAG.graph_id
+        from temp_authorized_graphs TAG
+		where walkthrough_id = p_walkthrough_id
+    ), all_valid_elements as (
+        select ELT.element_id 
+        from temp_walkthroughs TWA 
+		join sgraphs.elements ELT on TWA.element_id = ELT.element_id
         join all_authorized_graphs AAG on AAG.graph_id = ELT.graph_id
-		join sgraphs.periods PER on PER.period_id = ELT.element_period
-		where not sgraphs.are_periods_disjoin(p_period, PER.period_value)
-    ), relations_with_operands_in_elements (
-		-- pick relations having one operand in the valid elements, valid during period,
-		-- AND that are not already inserted. 
-		-- REMEMBER: we only want relations with a child in the valid entities ONLY
-		-- (otherwise we would load relations on relations) 
-		select  distinct RRO.relation_id, RRV.relation_value 
+    ), all_entities_to_delete as (
+		select TWA.element_id 
+		from temp_walkthroughs TWA
+		left outer join all_valid_elements AVE on AVE.element_id = TWA.element_id 
+		where AVE.element_id is null 
+	), all_relations_to_delete as (
+		select distinct TWA.element_id 
+		from temp_walkthroughs TWA
+		left outer join all_valid_elements AVE on AVE.element_id = TWA.element_operand 
+		where AVE.element_id is null 
+	)
+	delete from temp_walkthroughs TWD
+	where TWD.element_id in (
+		select AED.element_id 
+		from all_entities_to_delete
+		UNION ALL 
+		select ARD.element_id 
+		from all_relations_to_delete ARD 
+	);
+
+
+    with all_authorized_graphs as (
+        select TAG.graph_id
+        from temp_authorized_graphs TAG
+		where walkthrough_id = p_walkthrough_id
+    ), all_candidates_relations as (
+		-- pick relations having one operand in the cleaned base table 
+		-- AND that are not already inserted.
+		-- This step loads relations which childs are valid entities. Done once.  
+		select distinct RRO.relation_id, RRV.relation_value, RRV.relation_period_id as link_period 
 		from sgraphs.relation_role RRO
-		join sgraphs.elements ELT on ELT.element_id = RRO.relation_id 
-		join sgraphs.periods PER on PER.period_id = ELT.element_period
 		join sgraphs.relation_role_values RRV on RRV.relation_role_id = RRO.relation_role_id
-		join sgraphs.periods PERROL on PERROD.period_id = RRV.relation_period_id
-		join all_valid_elements AVE on AVE.element_id = RRV.relation_value
-		left outer join all_valid_elements EXAVE on EXAVE.element_id = RRO.relation_id
-		where EXAVE.element_id is null  
-		and not sgraphs.are_periods_disjoin(p_period, PER.period_value)
-		and not sgraphs.are_periods_disjoin(p_period, PERROL.period_value)
-		and AVE.is_entity = true -- entity childs only 
-	), relations_with_operands_graphs as (
-		-- This part is about operands that are visible or not. 
-		-- It contains relation id and childs, 
-		-- visible relation value is true if they are visible. 
-		-- Condition on time for value excludes inactive operands 
-		select RWOIE.relation_id, RRV.relation_value, 
-		(AAG.graph_id is not null) as visible_relation_value
-		from relations_with_operands_in_elements RWOIE 
-		join sgraphs.elements ELTCHILD on RWOIE.relation_value = ELTCHILD.element_id
-		join sgraphs.periods PERVAL on PERVAL.period_id = ELTCHILD.element_period
-		left outer join all_authorized_graphs AAG on AAG.graph_id = ELTCHILD.graph_id 
-		-- only restrict to elements that are active at that time 
-		where not sgraphs.are_periods_disjoin(p_period, PERVAL.period_value)
-	), visible_full_relations as (
-		-- get all relations with all visible operands from relations_with_operands_graphs. 
-		-- It means that those are all the relations with operands from initial walkthrough
-		-- that are active and with active operands, all of them visible (operands and relations)
-		select RWOG.relation_id, RWOG.relation_value 
-		from relations_with_operands_graphs RWOG
-		left outer join relations_with_operands_graphs RW on RW.relation_id = RWOG.relation_id 
-		where RW.relation_id is null 
-		and RW.visible_relation_value = false
+		join temp_walkthroughs TWA on TWA.element_id = RRV.relation_value -- operands are valid
+		left outer join temp_walkthroughs EXTWA on EXTWA.element_id = RRO.relation_id
+		where EXTWA.element_id is null  -- to find new elements, not already inserted   
+		and TWA.walkthrough_id = p_walkthrough_id
+		and EXTWA.walkthrough_id = p_walkthrough_id
+	), active_relations_with_operands as (
+		-- We do: 
+		-- NOT ask for operands to be active (for instance, X is the son of Y, and Y may not be active)
+		-- BUT WE WANT roles to be active, and relation to be active.  
+		-- We won't load recursively content of inactive data.
+		-- We also test that relation is visible.  
+		select  distinct ARC.relation_id, ARC.relation_value 
+		from all_candidates_relations ARC
+		join sgraphs.elements ELT on ELT.element_id = ARC.relation_id 
+		join sgraphs.periods PERLINK on ARC.link_period = PERLINK.period_id 
+		join sgraphs.periods PER on ELT.element_period = PER.period_id 
+		-- to restrict to visible graphs
+		join all_authorized_graphs AAG on AAG.graph_id = ELT.graph_id
+		where not sgraphs.are_periods_disjoin(p_period, PER.period_value)
+		and not sgraphs.are_periods_disjoin(p_period, PERLINK.period_value)
+	), active_relations_with_visible_operands as (
+		select ARWO.relation_id, ARWO.relation_value  
+		from active_relations_with_operands ARWO 
+		where ARWO.relation_id not in (
+			-- find relations with at least one NON visible operand. 
+			-- Test on periods was done before and that was a good idea. 
+			select ARWOIN.relation_id
+			from active_relations_with_operands ARWOIN
+			join sgraphs.elements ELTIN on ELTIN.element_id = ARWOIN.relation_value
+			left outer join all_authorized_graphs AAGIN on AAG.graph_id = ELTIN.graph_id 
+			where AAGIN.graph_id is null 
+		)
 	), new_elements_to_insert as (
 		select EPR.relation_id, EPR.relation_value 
-		from visible_full_relations VFR 
-		join temp_walkthroughs TWA on TWA.element_id = VFR.relation_id
+		from active_relations_with_visible_operands ARWVO 
+		join temp_walkthroughs TWA on TWA.element_id = ARWVO.relation_id
 		where TWA.element_id is null 
 		and TWA.walkthrough_id = p_walkthrough_id
 	)
@@ -179,45 +205,43 @@ begin
 			from susers.authorized_graphs AAG 
 			join susers.users USR on USR.user_id = AAG.auth_user_id 
 			where user_login = p_user_login
-    	), all_relation_candidates as (
-			-- get all relations that are active during period, in a visible graph, 
-			-- from previous iteration
-			select TW.element_operand as relation_id, RRV.relation_value
+    	), all_relation_operands_starter as (
+			-- find childs that are relation, active, to load from 
+			select TW.element_operand
 			from temp_walkthroughs TW 
+			-- pick only relations
 			join sgraphs.relation_role RRO on RRO.relation_id = TW.element_operand
-			join sgraphs.relation_role_value RRV on RRV.relation_role_id = RRO.relation_role_id
-			join sgraphs.periods PERROL on PERROL.period_id = RRV.relation_period_id
-			join sgraphs.elements ELT on ELT.element_id = TW.element_operand
-			join sgraphs.periods PER on PER.period_id = ELT.element_period
-			join all_authorized_graphs AGR on ELT.graph_id = AGR.graph_id -- element is visible
-			where walkthrough_id = p_walkthrough_id -- for that walkthrough
-			and element_operand is not null  -- child of relation, not relation 
-			and TW.height = (l_current_height - 1) -- coming from previous run
-			and not sgraphs.are_periods_disjoin(p_period, PER.period_value) -- active during period
-			and not sgraphs.are_periods_disjoin(p_period, PERROL.period_value)
-		), all_relations_operands (
-			-- from valid and visible relations, now we get their operands. 
-			-- Even if relation is valid, some operands may not be visible
-			select ARC.relation_id, ARC.relation_value, 
-			(AGR.graph_id is not null) as visible_operand 
-			from all_relation_candidates ARC
-			join sgraphs.elements ELT on ELT.element_id = ARC.relation_value 
+			join sgraphs.elements ELT on ELT.element_id = RRO.relation_id
+			-- find period for that relation  
 			join sgraphs.periods PER on PER.period_id = ELT.element_period 
-			left outer join  all_authorized_graphs AGR on ELT.graph_id = AGR.graph_id
+			-- restrict to active relations 
 			where not sgraphs.are_periods_disjoin(p_period, PER.period_value)
-		), visible_relations as (
-			-- visible relations define relations with all operands in visible graphs
-			select ARO.relation_id, ARO.relation_value 
-			from all_relations_operands ARO 
-			join all_relations_operands AROEX on AROEX.relation_id = ARO.relation_id 
-			where AROEX.relation_id  is null 
-			and AROEX.visible_operand = false
+			and TW.walkthrough_id = p_walkthrough_id
+			-- and are inserted last
+			and TW.height = l_max_previous_height
+		), all_active_candidates_relations as (
+			select AROS.element_operand as relation_id, RRV.relation_value
+			from all_relation_operands_starter AROS 
+			join sgraphs.relation_role_value RRV on RRV.relation_role_id = AROS.element_operand
+			join sgraphs.periods PER on PER.period_id = RRV.relation_period_id 
+			where not sgraphs.are_periods_disjoin(p_period, PER.period_value)
+		), all_active_visible_relations as (
+			select AACR.relation_id, AACR.relation_value 
+			from all_active_candidates_relations AACR
+			where AACR.relation_id not in (
+				select AACRIN.relation_id
+				from all_active_candidates_relations AACRIN
+				join sgraphs.elements ELTIN on ELTIN.element_id = AACRIN.relation_value  
+				left outer join all_authorized_graphs AAGIN on AAGIN.graph_id = ELTIN.graph_id
+				where AAGIN.graph_id is null 
+			)
 		), new_visible_relations as (
 			-- from visible relations, get only relations that were not inserted
-			select VRE.relation_id, VRE.relation_value 
-			from visible_relations VRE 
+			select AAVR.relation_id, AAVR.relation_value 
+			from all_active_visible_relations AAVR 
 			left outer join temp_walkthroughs TW on TW.element = VRE.relation_id
 			where TW.relation_id is null 
+			and TW.walkthrough_id = p_walkthrough_id
 		)
 		insert into temp_walkthroughs(walkthrough_id,element_id,element_operand,height)
 		select p_walkthrough_id, NVIR.relation_id, NVIR.relation_value, l_current_height 
